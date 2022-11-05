dcee1837a65c3c95a6fb79f16cb4fbc46cd29520
paligonshik <tadevosianvazgen@gmail.com>
2022-11-05 18:23:42 -0400

    added tester and files, more to go

---
+++ test_sample.py
@@ -0,0 +1,58 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "1de69db3",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import pytest"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "4283a54e",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def inc(x):\n",
+    "    return x + 1\n",
+    "\n",
+    "\n",
+    "def test_answer():\n",
+    "    assert inc(3) == 5\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "8856455a",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.13"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
