04abccef03a763318436ccbd9660dc4d99269df4
paligonshik <tadevosianvazgen@gmail.com>
2022-11-05 21:36:46 -0400

    in this commit we test 2 different examples in a list, you can run the same pytest test_sample1.py to see the results, you may see screen shots as well

--- test_sample1.py
+++ test_sample1.py
@@ -1,14 +1,51 @@
 from textblob import TextBlob
+import pytest
+# def extract_sentiment(text: str):
+#         '''Extract sentiment using textblob. 
+#         Polarity is within range [-1, 1]'''
 
-def extract_sentiment(text: str):
-        '''Extract sentiment using textblob. 
-        Polarity is within range [-1, 1]'''
+#         text = TextBlob(text)
 
-        text = TextBlob(text)
+#         return text.sentiment.polarity
 
-        return text.sentiment.polarity
+
+# def extract_sentiment(text: str):
+#         '''Extract sentiment using textblob. 
+#         Polarity is within range [-1, 1]'''
+
+#         text = TextBlob(text)
+
+#         return text.sentiment.polarity
+
+# def test_extract_sentiment():
+
+#     text = "I think this song is just a great song"
+
+#     sentiment = extract_sentiment(text)
+
+#     assert sentiment < 0
+
 
 #sentiment.py
+
+# def test_extract_sentiment_positive():
+
+#     text = "I think you will perform very best"
+
+#     sentiment = extract_sentiment(text)
+
+#     assert sentiment > 0
+
+# def test_extract_sentiment_negative():
+
+#     text = "I hate you, you dissapoitned me"
+
+#     sentiment = extract_sentiment(text)
+
+#     assert sentiment > 0
+
+
+
 def extract_sentiment(text: str):
         '''Extract sentiment using textblob. 
         Polarity is within range [-1, 1]'''
@@ -17,10 +54,12 @@ def extract_sentiment(text: str):
 
         return text.sentiment.polarity
 
-def test_extract_sentiment():
+testdata = ["I think today will be a great day","I do not think this will turn out well"]
 
-    text = "I think today will be a great day"
+#list of examples
+@pytest.mark.parametrize('sample', testdata) 
+def test_extract_sentiment(sample):
 
-    sentiment = extract_sentiment(text)
+    sentiment = extract_sentiment(sample)
 
-    assert sentiment < 0
+    assert sentiment > 0
